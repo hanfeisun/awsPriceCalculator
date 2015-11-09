b1a1855592ad2d80af871a2f1d059a4f257a57ca
Hanfei Sun <hanfei@waylens.com>
2015-11-09 22:20:33 +0800

    add delta and dependency highlight

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# aws_price
+
+
+# Parameters
+
+- *N<sub>u</sub>*: Users who has a waylens camera
+- *N<sub>m</sub>*: Monthly new *moments* per user
+- *T<sub>m</sub>*: Average length of video in a *moment*:
+- *N<sub>a</sub>*: Total audiences
+- *S*: Monthly sessions per audience per month
+>  A session is the period time an audience is actively engaged with the video website.
+
+- *T<sub>s</sub>*: Average length of video viewed per session:
+
+# Variables
+
+- *T<sub>up</sub>*: Total length of video uploaded per month
+>  *T<sub>up</sub>* = *N<sub>u</sub>* · *N<sub>m</sub>* · *T<sub>m</sub>*
+
+- *T<sub>acc</sub>*: Total length of video uploaded (accumulated)
+> *T<sub>acc</sub>* = ∑ *T<sub>up</sub>*
+
+- *T<sub>down</sub>*: Total length of video downloaded per month
+> *T<sub>down</sub>* = *N<sub>a</sub>* · *S* · *T<sub>s</sub>*
+
+# Cost Computation
+
+## Assumptions
+
+- Deploy to CDN only in United States
+> CDN cost in different region is different
+
+- The duration of each moment is integral multiple of 1 minute
+> Fractional minutes are rounded up when charged by Transcoder.
+
+- The cost for EC2 and its storage service(EBS) is constant
+>
+> EC2 is used for Web Service and Overlay Transcoding,
+> needs to scale out when *T<sub>up</sub>* is quite large
+>
+> EBS is used for VDB service,
+> needs to scale out when *T<sub>acc</sub>* is quite large
+
+
+
+## Notation
+
+For the function notation, see the line below:
+> Storage cost: *ß<sub>s</sub>*(*T<sub>acc</sub>*)
+
+Here I use *ß<sub>s</sub>* to refer to *Stoarge cost*. The following symbol wrapped by parenthesis means *ß<sub>s</sub>* depends on variable/parameter *T<sub>acc</sub>*.
+
+
+
+## Functions
+- S3 bucket
+  - Upload cost: *Zero*
+  > No cost if data is transferred from Amazon EC2
+
+  - Download cost: *Zero*
+  > No cost if data is transferred to Amazon Cloudfront (CDN).
+  > See Cloudfront Deploy cost below
+
+  - Storage cost: *ß<sub>s</sub>*(*T<sub>acc</sub>*)
+
+- Elastic Transcoder
+  - Transcoding: *E*(*T<sub>up</sub>*)
+
+- Cloudfront (CDN)
+  - Deploy cost: *C<sub>dep</sub>*(*T<sub>up</sub>*)
+  > Charged by "Data Transfer Out to Origin" (from S3 to CDN)
+
+  - Serve cost: *C<sub>srv</sub>*(*T<sub>down</sub>*)
+  > Charged by "Data Transfer Out to Internet"
+
+- EC2
+  - Server cost (constant): *EC*
+
+- Elastic Block Store (EBS)
+  - Storage cost (constant): *EBS*
+
+## Total Cost
+
+*Total*(*T<sub>acc</sub>* , *T<sub>up</sub>* , *T<sub>down</sub>*) = *ß<sub>s</sub>*(*T<sub>acc</sub>*) + *E*(*T<sub>up</sub>*) + *C<sub>dep</sub>*(*T<sub>up</sub>*) + *C<sub>srv</sub>*(*T<sub>down</sub>*) +
+*EC* + *EBS*
--- cal-compiled.js
+++ cal-compiled.js
@@ -2,11 +2,32 @@
 
 // Utils
 ;
-var $ = function (idSelector) {
-    return document.getElementById.apply(document, [idSelector.replace("#", "")]);
+var $id = function (idSelector) {
+    return document.getElementById(idSelector);
 },
-    parse = function (selector) {
-    return parseFloat($(selector).value);
+    $class = function (classSelector) {
+    return document.getElementsByClassName(classSelector);
+},
+    $get = function (node) {
+    if (typeof node === "string") {
+        node = $id(node);
+    }
+    if (node.nodeName == "INPUT") {
+        return parseFloat(node.value);
+    } else {
+        return parseFloat(node.innerText);
+    }
+},
+    $set = function (node, value) {
+    var result = value;
+    if (typeof node === "string") {
+        node = $id(node);
+    }
+    if (node.nodeName == "INPUT") {
+        node.value = result;
+    } else {
+        node.innerText = result;
+    }
 },
     step_minus = function (a, b) {
     if (a < b) {
@@ -16,10 +37,10 @@ var $ = function (idSelector) {
     }
 };
 
-var View = {
+var view = {
     subscribe: function (selector) {
         var self = this,
-            e = $(selector);
+            e = $id(selector);
 
         e.oninput = function () {
             self.model.notify();
@@ -29,36 +50,198 @@ var View = {
     },
     bind: function (selector, propFunc) {
         this.bindList[selector] = propFunc;
-    },
-    update: function () {
-        for (var selector in this.bindList) {
-            $(selector).innerText = this.model[this.bindList[selector]]().toFixed(2);
-        }
     }
 
 };
 
 var AwsView = function (awsModel) {
-    this.model = awsModel;
-    this.subscribe("#p_user");
-    this.subscribe("#p_user");
-    this.subscribe("#p_new_moment");
-    this.subscribe("#p_avg_length");
-    this.subscribe("#p_audience");
-    this.subscribe("#p_session");
-    this.subscribe("#p_session_length");
-    this.subscribe("#p_mbps");
-    this.bindList = {};
-    this.bind("#v_s3", "s3");
-    this.bind("#v_transcoder", "transcoder");
-    this.bind("#v_cdn", "cdn");
-    this.bind("#v_total", "total");
-    this.bind("#v_up", "d_upload_GB");
-    this.bind("#v_down", "d_download_GB");
-    this.bind("#v_up_c", "d_upload_cumulative_GB");
+    var self = this;
+
+    self.save = function () {
+        var elems = $class("model");
+        self.model.saved = {};
+
+        for (var i = 0; i < elems.length; i++) {
+            self.model.saved[elems[i].id] = $get(elems[i]);
+        }
+    };
+
+    self.restore = function () {
+        var saved = self.model.saved;
+        for (var prop in saved) {
+            if (saved.hasOwnProperty(prop)) {
+                $set(prop, saved[prop]);
+            }
+        }
+    };
+
+    self.compare = function () {
+        var saved = self.model.saved;
+        var transform = function (num, fixDecimal) {
+            var result;
+            result = num.toFixed(fixDecimal);
+            if (num > 0) {
+                result = "+" + result;
+            }
+            return result.replace("+", "↑ ").replace("-", "↓ ");
+        };
+
+        for (var prop in saved) {
+            if (saved.hasOwnProperty(prop)) {
+
+                var newVal = $get(prop),
+                    oldVal = saved[prop],
+                    delta = newVal - oldVal,
+                    e = $id(prop + "_delta");
+                if (prop.endsWith("unit")) {
+                    $set(e, transform(delta, 2));
+                } else {
+                    $set(e, transform(delta, 1));
+                }
+
+                if (delta === 0) {
+                    e.classList.remove("delta-inc");
+                    e.classList.remove("delta-dec");
+                } else if (delta > 0) {
+                    e.classList.add("delta-inc");
+                    e.classList.remove("delta-dec");
+                } else {
+                    e.classList.add("delta-dec");
+                    e.classList.remove("delta-inc");
+                }
+            }
+        }
+    };
+
+    self.update = function () {
+        for (var selector in self.bindList) {
+            if (self.bindList.hasOwnProperty(selector)) {
+                var computedProp = self.model[self.bindList[selector]]();
+                if (selector.endsWith("unit")) {
+                    $set(selector, computedProp.toFixed(2));
+                } else {
+                    $set(selector, computedProp.toFixed(1));
+                }
+            }
+        }
+        self.compare();
+    };
+
+    self.initDelta = function () {
+        var e,
+            delElem,
+            elems = $class("model");
+        for (var i = 0; i < elems.length; i++) {
+            e = elems[i];
+            delElem = document.createElement("span");
+            delElem.classList.add("delta");
+            delElem.id = e.id + "_delta";
+            delElem.innerText = "";
+            e.parentNode.insertBefore(delElem, e.nextSibling);
+        }
+        self.save();
+    };
+
+    self.bind3 = function (outputSelector, propFunc, inputSelectorArray) {
+        this.bind(outputSelector, propFunc);
+
+        for (var i = 0; i < inputSelectorArray.length; i++) {
+            // Hover input box
+            $id(inputSelectorArray[i]).addEventListener("mouseover", function () {
+                this.previousElementSibling.classList.add("related");
+                $id(outputSelector).previousElementSibling.classList.add("related");
+            });
+            $id(inputSelectorArray[i]).addEventListener("mouseout", function () {
+                this.previousElementSibling.classList.remove("related");
+                $id(outputSelector).previousElementSibling.classList.remove("related");
+            });
+
+            // Hover label of input box
+            $id(inputSelectorArray[i]).previousElementSibling.addEventListener("mouseover", function () {
+                this.classList.add("related");
+                $id(outputSelector).previousElementSibling.classList.add("related");
+            });
+
+            $id(inputSelectorArray[i]).previousElementSibling.addEventListener("mouseout", function () {
+                this.classList.remove("related");
+                $id(outputSelector).previousElementSibling.classList.remove("related");
+            });
+        }
+
+        // Hover output text
+        $id(outputSelector).addEventListener("mouseover", function () {
+            this.previousElementSibling.classList.add("related");
+            for (var i = 0; i < inputSelectorArray.length; i++) {
+                $id(inputSelectorArray[i]).previousElementSibling.classList.add("related");
+            }
+        });
+
+        $id(outputSelector).addEventListener("mouseout", function () {
+            this.previousElementSibling.classList.remove("related");
+            for (var i = 0; i < inputSelectorArray.length; i++) {
+                $id(inputSelectorArray[i]).previousElementSibling.classList.remove("related");
+            }
+        });
+
+        // Hover label of output text
+        $id(outputSelector).previousElementSibling.addEventListener("mouseover", function () {
+            this.classList.add("related");
+            for (var i = 0; i < inputSelectorArray.length; i++) {
+                $id(inputSelectorArray[i]).previousElementSibling.classList.add("related");
+            }
+        });
+
+        $id(outputSelector).previousElementSibling.addEventListener("mouseout", function () {
+            this.classList.remove("related");
+            for (var i = 0; i < inputSelectorArray.length; i++) {
+                $id(inputSelectorArray[i]).previousElementSibling.classList.remove("related");
+            }
+        });
+    };
+
+    self.model = awsModel;
+    self.subscribe("p_user");
+    self.subscribe("p_new_moment");
+    self.subscribe("p_avg_length");
+    self.subscribe("p_audience");
+    self.subscribe("p_session");
+    self.subscribe("p_session_length");
+    self.subscribe("p_mbps");
+    self.bindList = {};
+
+    self.bind3("v_up", "d_upload_GB", ["p_user", "p_new_moment", "p_avg_length", "p_mbps"]);
+    self.bind3("v_up_c", "d_upload_cumulative_GB", ["p_user", "p_new_moment", "p_avg_length", "p_mbps", "v_up"]);
+    self.bind3("v_down", "d_download_GB", ["p_audience", "p_session", "p_session_length", "p_mbps"]);
+
+    self.bind3("v_s3", "s3", ["p_user", "p_new_moment", "p_avg_length", "p_mbps", "v_up_c", "v_s3_unit"]);
+    self.bind3("v_transcoder", "transcoder", ["p_user", "p_new_moment", "p_avg_length", "p_mbps", "v_up"]);
+    self.bind3("v_cdn", "cdn", ["p_audience", "p_session", "p_session_length", "p_mbps", "v_down", "v_up"]);
+
+    self.bind3("v_s3_unit", "s3_unit", []);
+    self.bind3("v_transcoder_unit", "transcoder_unit", ["p_mbps"]);
+    self.bind3("v_cdn_unit", "cdn_unit", ["p_audience", "p_session", "p_session_length", "p_mbps", "v_down", "v_up"]);
+
+    self.bind3("v_total", "total", ["v_s3", "v_transcoder", "v_cdn"]);
+
+    $id("b_delta").addEventListener("click", function () {
+        var elements = $class("delta");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.toggle("hide");
+        }
+    });
+
+    $id("b_save").addEventListener("click", function () {
+        self.save();
+        self.compare();
+    });
+
+    $id("b_restore").addEventListener("click", function () {
+        self.restore();
+        self.compare();
+    });
 };
 
-AwsView.prototype = View;
+AwsView.prototype = view;
 
 var AwsModel = function () {
     this.notify = this.parseAll;
@@ -66,13 +249,13 @@ var AwsModel = function () {
 };
 
 AwsModel.prototype.parseAll = function () {
-    this.p_user = parse("#p_user");
-    this.p_new_moment = parse("#p_new_moment");
-    this.p_avg_minute = parse("#p_avg_length");
-    this.p_audience = parse("#p_audience");
-    this.p_session = parse("#p_session");
-    this.p_session_minute = parse("#p_session_length");
-    this.p_mbps = parse("#p_mbps");
+    this.p_user = $get("p_user");
+    this.p_new_moment = $get("p_new_moment");
+    this.p_avg_minute = $get("p_avg_length");
+    this.p_audience = $get("p_audience");
+    this.p_session = $get("p_session");
+    this.p_session_minute = $get("p_session_length");
+    this.p_mbps = $get("p_mbps");
 };
 
 AwsModel.prototype.t_upload_minute = function () {
@@ -113,9 +296,17 @@ AwsModel.prototype.s3 = function () {
     return 0.13 * this.d_upload_cumulative_GB();
 };
 
+AwsModel.prototype.s3_unit = function () {
+    return 0.13;
+};
+
 AwsModel.prototype.transcoder = function () {
     // https://aws.amazon.com/elastictranscoder/pricing/
-    return 0.03 * this.d_upload_GB();
+    return 0.03 * this.t_upload_minute();
+};
+
+AwsModel.prototype.transcoder_unit = function () {
+    return this.transcoder() / this.d_upload_GB();
 };
 
 AwsModel.prototype.cdn = function () {
@@ -123,10 +314,13 @@ AwsModel.prototype.cdn = function () {
         down_size = this.d_download_GB();
 
     // https://aws.amazon.com/cloudfront/pricing/
-
     var up_price = up_size * 0.02,
         down_price = Math.min(down_size, 10000) * 0.085 + Math.min(step_minus(down_size, 10000), 40000) * 0.08 + Math.min(step_minus(down_size, 50000), 100000) * 0.06 + Math.min(step_minus(down_size, 150000), 350000) * 0.04 + Math.min(step_minus(down_size, 500000), 524000) * 0.03 + Math.min(step_minus(down_size, 1024000), 4000000) * 0.025 + step_minus(down_size, 5024000) * 0.020;
     return up_price + down_price;
+};
+
+AwsModel.prototype.cdn_unit = function () {
+    return this.cdn() / this.d_download_GB();
 };
 
 AwsModel.prototype.total = function () {
